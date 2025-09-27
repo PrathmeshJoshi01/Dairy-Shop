1ba3a9474cd3379fd4b85f4582af5ccf1826d6eb
PrathmeshJoshi01 <joshiprathmeshofficial1952@gmail.com>
2025-09-27 14:34:39 +0530

    Initial commit

--- joshi's dairy farm/feed-service/feed-service/src/main/java/com/joshisFarm/feed_service/FeedServiceApplication.java
+++ joshi's dairy farm/feed-service/feed-service/src/main/java/com/joshisFarm/feed_service/FeedServiceApplication.java
@@ -2,8 +2,10 @@ package com.joshisFarm.feed_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.joshisFarm.feed_service.client") 
 public class FeedServiceApplication {
 
 	public static void main(String[] args) {
